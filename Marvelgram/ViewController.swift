e068fe79e5566f6b35a1591e7a58d7a8bd3acded
nevzorov46 <masterpiece.nevzorov@gmail.com>
2021-08-02 20:18:31 +0300

    Initial Commit

---
+++ Marvelgram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Marvelgram
+//
+//  Created by Иван Карамазов on 02.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
